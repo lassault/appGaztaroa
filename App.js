455ec1dd73c0249c6d4f614c78dfda9cb77b2e0f
lassa97 <flassai@gmail.com>
2021-04-15 20:03:14 +0200

    Primeros pasos en React Native

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello world!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +14,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, 
+    backgroundColor: '#e4e4e4',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#888',
+    fontSize: 25
+  },
+  text: {
+    color: '#888',
+    fontSize: 18
+  }
 });
